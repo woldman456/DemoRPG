3c3f6eb7bfb88521e5eae0ddcd2db96ca68a3dbb
Paul Shook <pshook456@gmail.com>
2021-11-28 00:24:06 -0600

    additional reformatting

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,34 @@ public class Main {
 
     public static Scanner s = new Scanner(System.in);
     static boolean play = true;
-
-
+    static String name = "";
+    static Player player = new Player(10, name);
+    static Functions functions = new Functions();
+    static Scene scene = new Scene();
 
 
     public static void main(String[] args) {
         startOfGame();
     }
 
-    public static void startOfGame(){
-        Functions functions = new Functions();
+    public static void startOfGame() {
         functions.gatherInformation();
         String name = s.next();
-        Player player = new Player(10, name);
-        Scene scene = new Scene();
+
         int choice = 0;
 
 
-        while (play || player.getHealth() == 0) {
+        while (play || player.getHealth() > 0) {
 
             System.out.println("Hello would you like to play a game");
             System.out.print("If yes press one, if No press two: ");
             choice = s.nextInt();
             switch (choice) {
                 case 1:
-
-
                     System.out.println("Great " + name + "lets get going!");
-
                     scene.sceneOne();
                     choice = s.nextInt();
-                    switch (choice){
+                    switch (choice) {
                         case 1:
                             scene.sceneTwo();
                             choice = s.nextInt();
@@ -51,7 +48,7 @@ public class Main {
                         case 2:
                             scene.sceneThree();
                             choice = s.nextInt();
-                            switch (choice){
+                            switch (choice) {
                                 case 1:
                                     scene.roadOne();
                                 case 2:
@@ -60,7 +57,6 @@ public class Main {
                                     scene.sceneTwo();
                             }
                     }
-
 
 
                 case 2:
